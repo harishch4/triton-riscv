194705e39fa9510b00f1b2355eb47a66e1696f0c
harish <harishcse44@gmail.com>
2024-05-25 11:59:20 +0530

    kernel invokation code

--- launch.cpp
+++ launch.cpp
@@ -1,7 +1,7 @@
 #include "ExecutionEngine/CRunnerUtils.cpp"
 #include "ExecutionEngine/CRunnerUtils.h"
-#include <Python.h>
 #include <assert.h>
+#include <iostream>
 #include <stdbool.h>
 
 extern "C" {
@@ -18,6 +18,7 @@ static void _launch(int gridX, int gridY, int gridZ, void *arg0, void *arg1,
     for (int x = 0; x < gridX; x++) {
       for (int y = 0; y < gridY; y++) {
         for (int z = 0; z < gridZ; z++) {
+          std::cout << "(" << x << ", " << y << ", " << z << ")\n";
           // Use some random type "char" here.
           StridedMemRefType<char, 0> ptr_arg0 = {static_cast<char *>(arg0),
                                                  static_cast<char *>(arg0), 0};
@@ -25,6 +26,11 @@ static void _launch(int gridX, int gridY, int gridZ, void *arg0, void *arg1,
                                                  static_cast<char *>(arg1), 0};
           StridedMemRefType<char, 0> ptr_arg2 = {static_cast<char *>(arg2),
                                                  static_cast<char *>(arg2), 0};
+          // for (int i = 0; i < arg3; i++) {
+          //   std::cout << "(" << ptr_arg0.data[i] << ", " << ptr_arg1.data[i]
+          //             << ", " << ptr_arg2.data[i] << ")\n";
+          // }
+
           add_kernel(0, &ptr_arg0, 0, &ptr_arg1, 0, &ptr_arg2,
                      static_cast<int32_t>(arg3), gridX, gridY, gridZ, x, y, z);
         }
@@ -33,134 +39,24 @@ static void _launch(int gridX, int gridY, int gridZ, void *arg0, void *arg1,
   }
 }
 
-typedef struct _DevicePtrInfo {
-  void *dev_ptr;
-  bool valid;
-} DevicePtrInfo;
-
-static inline DevicePtrInfo getPointer(PyObject *obj, int idx) {
-  DevicePtrInfo ptr_info;
-  ptr_info.dev_ptr = 0;
-  ptr_info.valid = true;
-  if (PyLong_Check(obj)) {
-    ptr_info.dev_ptr = reinterpret_cast<void *>(PyLong_AsUnsignedLongLong(obj));
-    return ptr_info;
-  }
-  if (obj == Py_None) {
-    // valid nullptr
-    return ptr_info;
-  }
-  PyObject *ptr = PyObject_GetAttrString(obj, "data_ptr");
-  if (ptr) {
-    PyObject *empty_tuple = PyTuple_New(0);
-    PyObject *ret = PyObject_Call(ptr, empty_tuple, NULL);
-    Py_DECREF(empty_tuple);
-    Py_DECREF(ptr);
-    if (!PyLong_Check(ret)) {
-      PyErr_SetString(
-          PyExc_TypeError,
-          "data_ptr method of Pointer object must return 64-bit int");
-      ptr_info.valid = false;
-      return ptr_info;
-    }
-    ptr_info.dev_ptr = reinterpret_cast<void *>(PyLong_AsUnsignedLongLong(ret));
-    if (!ptr_info.dev_ptr)
-      return ptr_info;
-    Py_DECREF(ret); // Thanks ChatGPT!
-    return ptr_info;
-  }
-  PyErr_SetString(
-      PyExc_TypeError,
-      "Pointer argument must be either uint64 or have data_ptr method");
-  return ptr_info;
-}
-
-static PyObject *launch(PyObject *self, PyObject *args) {
+int main() {
+  std::cout << "Inside main\n";
   int gridX, gridY, gridZ;
-  PyObject *launch_enter_hook = NULL;
-  PyObject *launch_exit_hook = NULL;
-  PyObject *kernel_metadata = NULL;
-  PyObject *launch_metadata = NULL;
-  PyObject *_arg0;
-  PyObject *_arg1;
-  PyObject *_arg2;
-  int32_t _arg3;
-  if (!PyArg_ParseTuple(args, "iiiOOOOOOOi", &gridX, &gridY, &gridZ,
-                        &kernel_metadata, &launch_metadata, &launch_enter_hook,
-                        &launch_exit_hook, &_arg0, &_arg1, &_arg2, &_arg3)) {
-    return NULL;
+  int *arg0, *arg1, *arg2;
+  int arg3;
+  gridX = gridY = gridZ = 2;
+  arg3 = 1024;
+  arg0 = (int *)malloc(arg3 * sizeof(int));
+  arg1 = (int *)malloc(arg3 * sizeof(int));
+  arg2 = (int *)malloc(arg3 * sizeof(int));
+  for (int i = 0; i < arg3; i++) {
+    arg0[i] = i;
+    arg1[i] = i;
+    arg2[i] = i;
   }
-
-  // [CPULauncher-specific]: We don't need the metadata below but just put them
-  // here anyway to be consistent with others.
-  // This will make updating the driver easier in the future.
-
-  //  int num_warps, num_ctas, shared_memory, clusterDimX, clusterDimY,
-  //  clusterDimZ; if (!PyArg_ParseTuple(kernel_metadata, "iiiiii", &num_warps,
-  //  &num_ctas, &shared_memory, &clusterDimX, &clusterDimY, &clusterDimZ)) {
-  //    PyErr_SetString(PyExc_TypeError, "kernel_metadata must be a tuple");
-  //    return NULL;
-  //  }
-
-  // extract launch metadata
-  if (launch_enter_hook != Py_None) {
-    PyObject *args = Py_BuildValue("(O)", launch_metadata);
-    PyObject *ret = PyObject_CallObject(launch_enter_hook, args);
-    Py_DECREF(args);
-    if (!ret)
-      return NULL;
+  _launch(gridX, gridY, gridZ, arg0, arg1, arg2, arg3);
+  std::cout << "Done\n";
+  for (int i = 0; i < arg3; i++) {
+    std::cout << "(" << arg0[i] << ", " << arg1[i] << ", " << arg2[i] << ")\n";
   }
-
-  // raise exception asap
-  DevicePtrInfo ptr_info0 = getPointer(_arg0, 0);
-  if (!ptr_info0.valid)
-    return NULL;
-  ;
-  DevicePtrInfo ptr_info1 = getPointer(_arg1, 1);
-  if (!ptr_info1.valid)
-    return NULL;
-  ;
-  DevicePtrInfo ptr_info2 = getPointer(_arg2, 2);
-  if (!ptr_info2.valid)
-    return NULL;
-  ;
-  ;
-  _launch(gridX, gridY, gridZ, ptr_info0.dev_ptr, ptr_info1.dev_ptr,
-          ptr_info2.dev_ptr, _arg3);
-
-  if (PyErr_Occurred()) {
-    return NULL;
-  }
-  if (launch_exit_hook != Py_None) {
-    PyObject *args = Py_BuildValue("(O)", launch_metadata);
-    PyObject *ret = PyObject_CallObject(launch_exit_hook, args);
-    Py_DECREF(args);
-    if (!ret)
-      return NULL;
-  }
-
-  // return None
-  Py_INCREF(Py_None);
-  return Py_None;
-}
-
-static PyMethodDef ModuleMethods[] = {
-    {"launch", launch, METH_VARARGS,
-     "Entry point for all kernels with this signature"},
-    {NULL, NULL, 0, NULL} // sentinel
-};
-
-static struct PyModuleDef ModuleDef = {
-    PyModuleDef_HEAD_INIT, "__triton_shared_ref_cpu_kernel_launcher",
-    NULL, // documentation
-    -1,   // size
-    ModuleMethods};
-
-PyMODINIT_FUNC PyInit___triton_shared_ref_cpu_kernel_launcher(void) {
-  PyObject *m = PyModule_Create(&ModuleDef);
-  if (m == NULL) {
-    return NULL;
-  }
-  PyModule_AddFunctions(m, ModuleMethods);
-  return m;
 }
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-# triton-riscv
 rm *.o a.out
 llc -march=riscv64 -mattr=+d kernel_riscv.ll -filetype=obj
-riscv64-unknown-linux-gnu-g++ -c launch.cpp
+riscv64-unknown-linux-gnu-g++ -c -I include launch.cpp
 riscv64-unknown-linux-gnu-g++ -static launch.o kernel_riscv.o
 qemu-riscv64-static a.out
